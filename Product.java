24344650fa3f1dff595834f189d0203331170601
KevinStrong <narcindin@gmail.com>
2017-12-28 17:02:27 -0600

    Initial commit

---
+++ Product.java
@@ -0,0 +1,48 @@
+package vendingmachine;
+
+/**
+ *
+ * @author Kevin
+ */
+class Product {
+    int price;
+    int quantity;
+    String name;
+    
+    Product(String name, int price, int quantity)
+    {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+    protected String getName()
+    {
+        return name;
+    }
+    protected int getQuantity()
+    {
+        return quantity;
+    }
+    protected int getPrice()
+    {
+        return price;
+    }
+    protected boolean inStock()
+    {
+        return quantity > 0;
+    }
+    protected void buy()
+    {
+        quantity--;
+    }
+    
+    //Employee Methods
+    protected void setPrice(int newprice)
+    {
+        price = newprice;
+    }
+    protected void restock(int ammount)
+    {
+        quantity+= ammount;
+    }
+}
